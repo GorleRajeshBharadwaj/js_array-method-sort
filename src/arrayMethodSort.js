b803e87cfb08d113b83ff290143c5a20b5c082c1
RajeshBharadwaj <32881036+GorleRajeshBharadwaj@users.noreply.github.com>
2026-01-18 15:20:14 +0530

    Update arrayMethodSort.js

--- src/arrayMethodSort.js
+++ src/arrayMethodSort.js
@@ -6,7 +6,20 @@
 function applyCustomSort() {
   [].__proto__.sort2 = function(compareFunction) {
     // write code here
+    for (let i = this.length; i > 1; i--) {
+      for(let j = 0; j < i; j++) {
+        if (compareFunction(this[j], this[i]) < 0) {
+          swap(this, i, j);
+        }
+      }
+    }
   };
+}
+
+function swap(arr, firstElementIndex, secondElementIndex) {
+  const tempPlaceHolder = arr[firstElementIndex];
+  arr[firstElementIndex] = arr[secondElementIndex];
+  arr[secondElementIndex] = tempPlaceHolder;
 }
 
 module.exports = applyCustomSort;
